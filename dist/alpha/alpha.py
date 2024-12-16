ed30896e3cf504bde4af84b6064038bec5b2e21d
Griffin Kiesecker <gkiesecker@tekpossible.com>
2024-12-15 23:30:30 -0700

    Working on internal queue for alpha servers. I will work on it more since right now I have not sent any messages with the queue

--- dist/alpha/alpha.py
+++ dist/alpha/alpha.py
@@ -3,6 +3,8 @@ import socket
 import threading
 import json
 import time
+import uuid
+
 class AlphaServer:
     def __init__(self, hostname, internal_status_port, internal_queue_port, external_queue_port_send, external_queue_port_recv, bravo_queue_port_send, bravo_queue_port_recv  ):
         self.hostname = hostname
@@ -16,6 +18,9 @@ class AlphaServer:
         self.alpha_secret = "ABCDEFG"
         self.bravo_remote_loadbalancer = ""
         self.lock = None    
+        self.msg_queue = []
+        self.queue_updated = None
+
     def generate_status_message(self):
         status_message = {
             "hostname": self.hostname,
@@ -26,6 +31,44 @@ class AlphaServer:
 
         }
         return(json.dumps(status_message))
+    
+    # def generate_queue_message(self, queue_data):
+
+    def start_queue_recv(self):
+        queue_listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        queue_listener.bind(('0.0.0.0', self.internal_queue_port))
+        queue_listener.listen(100)
+        while True:
+            (client_socket, address) = queue_listener.accept()
+            print("Connection <<INT-QUEUE>> Started: " + str(address))
+            thread = threading.Thread(self.queue_handler_recv(client_socket, address))
+
+    def queue_handler_recv(self, client_socket: socket.socket, address):
+        while True:
+            self.lock.acquire()
+            queue_msg = client_socket.recv(4096)
+            self.msg_queue.append(json.loads(str(queue_msg.decode())))
+            queue_msg.send("ACK")
+            self.lock.release()
+
+    def start_queue_send(self):
+        while True:
+            if self.queue_updated != None:
+                self.lock.acquire()
+                self.msg_queue.append(self.queue_updated)
+                for alpha_server in self.alpha_servers:
+                    if alpha_server['isReady']:
+                        send_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                        send_socket.connect(( alpha_server['ip_address'], self.internal_queue_port))
+                        response = send_socket.recv(4096)
+                        if "ACK" in str(response.decode()):
+                            send_socket.close()
+                        else:
+                            print("Error in sending queue update to server " + alpha_server['ip_address'] + "!")
+                            send_socket.close()
+                self.queue_updated = None
+                self.lock.release()
+            time.sleep(5) # At the moment, wait 5 seconds 
 
     # def get_alpha_secret(): TODO: We will need to write a AWS secret manager call here once we are cloud ready
     def get_ip_address(self):
@@ -45,12 +88,7 @@ class AlphaServer:
 
     def recv_client_status(self, client_socket: socket.socket, address):
         client_socket.send(bytes(self.generate_status_message(), 'utf-8'))
-        client_data = client_socket.recv(4096) # RECEIVE UP TO 4096 BYTES
-
-        try: # Make sure the data is valid json
-            client_status: str = json.loads(str(client_data.decode()))
-        except:
-            x = 0 # Error parsing the JSON object that was sent to us
+        time.sleep(0.2)
         print("Connection <<STATUS>> Ended: " + str(address))
         client_socket.close()
 
@@ -69,10 +107,11 @@ class AlphaServer:
                             client_socket.settimeout(0.01)  
                             client_socket.connect((current_ip, self.internal_status_port))
                             client_data = client_socket.recv(4096) # RECEIVE UP TO 4096 BYTES
+                            client_socket.close()
                             try: # Make sure the data is valid json
                                 client_status = json.loads(str(client_data.decode()))
-                                self.lock.acquire()
                                 is_duplicate =  False
+                                self.lock.acquire()
                                 for i in range(len(self.alpha_servers)):
                                     if self.alpha_servers[i]["hostname"] == client_status["hostname"]:
                                         is_duplicate = True
@@ -99,9 +138,11 @@ class AlphaServer:
         cluster_listener.start()
         cluster_search = threading.Thread(target=self.find_servers)
         cluster_search.start()
+        queue_recv = threading.Thread(target=self.start_queue_recv)
+        queue_recv.start()
+        queue_send = threading.Thread(target=self.start_queue_send)
+        queue_send.start()
         self.alpha_servers.append({"hostname": self.hostname, "ip_address": self.get_ip_address(), "isReady": self.isReady()})
         while True:
-            self.lock.acquire()
             print("Currently running with following alpha_servers: " + str(self.alpha_servers))
-            self.lock.release()
             time.sleep(10)
